00ae0dfa1cad44c96e23b7cdb5787f3435bf82aa
zhangxiaodong <zhangxiaodong@daojia.com.cn>
2018-12-05 19:58:33 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import Xui from 'healen-x-ui'
 import 'healen-x-ui/packages/x-css/lib/index.css'
 Vue.use(Xui)
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
 
